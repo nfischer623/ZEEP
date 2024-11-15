363f375c690c438956db8879ddfdcf35c963f32b
nfischer623 <90053357+nfischer623@users.noreply.github.com>
2024-11-14 23:31:38 -0500

    Added button functionality from the tutorial.

--- FXDemo/src/main/java/com/example/fxdemo/Main.java
+++ FXDemo/src/main/java/com/example/fxdemo/Main.java
@@ -1,14 +1,18 @@
 package com.example.fxdemo;
 
 import javafx.application.Application;
-import javafx.geometry.Insets;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import javafx.scene.control.Label;
 
 public class Main extends Application {
 //    @Override
@@ -19,22 +23,57 @@ public class Main extends Application {
 //        stage.setScene(scene);
 //        stage.show();
 //    }
-    @Override
-    public void start(Stage primaryStage) {
-        Scene scene = new Scene(createContents(),400,200);
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
-
     public static void main(String[] args) {
         launch(args);
     }
 
-    private Region createContents() {
-        HBox results = new HBox(new Label("Name:"), new TextField(""));
-        results.setSpacing(6);
-        results.setPadding(new Insets(0,0,0,50));
-        results.setAlignment(Pos.CENTER_LEFT);
+    private final StringProperty greeting = new SimpleStringProperty("");
+    private final StringProperty name = new SimpleStringProperty("");
+
+    @Override
+    public void start(Stage primaryStage) {
+        Scene scene = new Scene(createContent(),400,200);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    private Region createContent() {
+        TextField inputTextField = new TextField("");
+        HBox inputRow = new HBox(new Label("Name:"), inputTextField);
+        inputRow.setSpacing(6);
+        inputRow.setAlignment(Pos.CENTER);
+
+        Label outputLabel = new Label("");
+        Button actionButton = new Button("Hello");
+        actionButton.setOnAction(evt -> outputLabel.setText("Hello " + inputTextField.getText()));
+
+        VBox results = new VBox(20, inputRow, outputLabel, actionButton);
+        results.setAlignment(Pos.CENTER);
+
         return results;
+    }
+
+    private Node createGreetingButton() {
+        Button results = new Button("Hello");
+        results.setOnAction(evt -> setGreeting());
+        return results;
+    }
+
+    private Node createInputRow() {
+        TextField textField = new TextField("");
+        textField.textProperty().bindBidirectional(name);
+        HBox hBox = new HBox(6, new Label("Name:"), textField);
+        hBox.setAlignment(Pos.CENTER);
+        return hBox;
+    }
+
+    private Node createOutputLabel() {
+        Label results = new Label("");
+        results.textProperty().bind(greeting);
+        return results;
+    }
+
+    private void setGreeting() {
+        greeting.set("Hello " + name.get());
     }
 }
